b9b1d8be76f2b12b294838c4c27bdfee4983fd15
Maycon Vieira <93644599+MayconVMendes@users.noreply.github.com>
2022-06-20 18:46:27 -0300

    mais sobre readonly CORRECT

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,12 +1,12 @@
 export class Negociacao {
 
     constructor(
-        public readonly _data: Date, 
-        public readonly _quantidade: number, 
+        public readonly data: Date, 
+        public readonly quantidade: number, 
         public readonly _valor: number
     ) {}
 
     get volume(): number{
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     }
 }
