a525cb1f7054d35d00fae0dae21e1a7d5e9ab5a8
Maycon Vieira <93644599+MayconVMendes@users.noreply.github.com>
2022-06-20 18:32:00 -0300

    readonlyArray array de somente leitura

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,4 +3,12 @@ export class Negociacoes {
 
     private negociacoes: Array<Negociacao> = [];
 
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): ReadonlyArray<Negociacao> {
+        return this.negociacoes;
+    }
+
 }
