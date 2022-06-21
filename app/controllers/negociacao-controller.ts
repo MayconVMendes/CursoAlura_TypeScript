f71dcc3edd2acbc4263b986465bb477db5689e7a
Maycon Vieira <93644599+MayconVMendes@users.noreply.github.com>
2022-06-21 12:04:43 -0300

    decorators teste de tempo de execucao

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -3,6 +3,7 @@ import { NegociacoesView } from './../views/negociacoes-view.js';
 import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from './../models/negociacao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
@@ -17,6 +18,8 @@ export class NegociacaoController {
         this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
+
+    @logarTempoDeExecucao()
 
     public adiciona(): void {
 
